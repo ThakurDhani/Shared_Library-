b64adc67a9ca26ae007751718930237fa54cc7d1
ThakurDhani <thakurdhani143@gmail.com>
2025-07-24 01:15:23 +0530

    Update updateIndex.groovy

--- vars/updateIndex.groovy
+++ vars/updateIndex.groovy
@@ -2,8 +2,8 @@ def call(String name) {
     // Get current date & time
     def now = new Date()
     def tz = TimeZone.getTimeZone("Asia/Kolkata")
-    def date = now.format("yyyy-MM-dd")
-    def time = now.format("HH:mm:ss")
+    def date = now.format("yyyy-MM-dd", tz)
+    def time = now.format("HH:mm:ss", tz)
     // Load HTML template from resources
     def template = libraryResource('index.html')
     // Replace placeholders
